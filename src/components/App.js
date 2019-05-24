80c9cff0bae6f08b63ed27ce47dedd96876d7be8
deeseert <gio.amms@gmail.com>
2019-05-24 11:40:51 +0100

    added Navbar

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar
+        title="I'm the Navbar"
+        subtitle="Put here other stuff"
+        colour="orange"
+        icon="american sign language interpreting"
+      />
+    </div>
+  );
+}
+
+export default App;
