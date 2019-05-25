d86a7867949e787deaea62ad2a3ee60f69a4ae4f
sam20gh <sam20gh@gmail.com>
2019-05-25 20:54:02 +0100

    Nav Bar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,13 @@ import React from "react";
 // import Search from "./Search";
 
 const Navbar = props => (
-  <div className={`ui inverted ${props.colour} menu`}>
-    <a className="item">
-      <h2 className="ui header">
-        <i className={`${props.icon} icon`} />
-        <div className="content">{props.title}</div>
-        <div className="sub header">{props.subtitle}</div>
-      </h2>
-    </a>
-    <div>{/* <Search handleSearch={props.handleSearch} /> */}</div>
+  <div class="ui stackable menu">
+    <div class="item">
+      <img src="https://react.semantic-ui.com/logo.png" />
+    </div>
+    <a class="item">Features</a>
+    <a class="item">Testimonials</a>
+    <a class="item">Sign-in</a>
   </div>
 );
 
