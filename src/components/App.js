89520c1bd3cf216cc1f1b3f33a6cfb0a30843635
sam20gh <sam20gh@gmail.com>
2019-05-28 15:38:42 +0100

    Products Page completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,20 @@
-import React from "react";
-import Navbar from "./Navbar";
+import React, { Component } from 'react';
 
-function App() {
-  return (
+import { Route, Switch } from "react-router-dom";
+
+import Navbar from "./Navbar";
+import ProductList from "./ProductList";
+import ProductDetails from "./ProductDetails";
+
+class App extends Component {
+
+  state = {
+    searchTerm: "",
+    products: []
+  }
+
+  render() { 
+    return (
     <div className="App">
       <Navbar
         title="I'm the Navbar"
@@ -10,8 +22,25 @@ function App() {
         colour="orange"
         icon="american sign language interpreting"
       />
+      {/* Switch statement in order to create a 404 Page only in case the URL is wrong */}
+      <Switch>
+        <Route exact path="/" component={prop => <h1>Home Page</h1>} />
+        <Route exact path="/products" component={props => <ProductList {...props} />} />
+        {/* This below is the prop that will have id in params */}
+        <Route exact path="/products/:id" component={props => {
+          const id = props.match.params.id
+          const product = this.state.products.find(product =>product.id === id)
+          // console.log(id)
+          if (this.state.products === 0) return <h1>Loading...</h1>
+          if (this.state.products > 0 && product === undefined) return <h1>Product not found bro!</h1>  
+          
+          return <ProductDetails {...props} product={product} />
+        }} />
+        <Route component={props => <h1>404 - not Found</h1>} />
+      </Switch>
     </div>
   );
+  }
 }
-
+ 
 export default App;
